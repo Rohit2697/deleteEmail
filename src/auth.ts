cc73d412b47896625005d54199aedc81defebdc7
Rohitdey2697 <rohit.dey2697@gmail.com>
2025-08-16 09:38:19 +0530

    added batchDelete functionality

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { authenticate } from '@google-cloud/local-auth';
 
 const SCOPES = ['https://mail.google.com/'];
-const TOKEN_PATH = path.join(process.cwd(), 'token_bittudey28.json');
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 async function main() {
